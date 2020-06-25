2b681722d725b84adf32746180d54e2076bdb51c
UpperCod <mtrujillo@outlook.cl>
2020-06-25 00:47:07 -0400

    Ready!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 import "./components/atomico-brand/atomico-brand";
-import "./components/atomico-header/atomico-header";
-import "./components/atomico-halo/atomico-halo";
-import "./components/app-container/app-container";
-import "./components/app-header/app-header";
-import "./components/app-avatar/app-avatar";
-import "./components/app-gallery/app-gallery";
