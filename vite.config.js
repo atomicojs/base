cfdbb76dd1985a84203e48594950609e94d26656
UpperCod <mtrujillo@outlook.cl>
2021-04-21 16:36:32 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 export default {
   esbuild: {
-    jsxFactory: "h",
-    jsxInject: `import {h} from 'atomico'`,
+    jsxFactory: "_jsx",
+    jsxInject: `import {jsx as _jsx} from 'atomico/jsx-runtime'`,
   },
 };
