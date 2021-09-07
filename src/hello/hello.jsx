be5d12326c1cb302d543f323d961045f6b46d813
UpperCod <mtrujillo@outlook.cl>
2021-09-07 15:03:53 -0300

    remove hello.css

--- src/hello/hello.jsx
+++ src/hello/hello.jsx
@@ -1,5 +1,4 @@
-import { c } from "atomico";
-import style from "./hello.css";
+import { c, css } from "atomico";
 
 function hello({ message }) {
   return (
@@ -19,6 +18,34 @@ hello.props = {
   },
 };
 
-hello.styles = style;
+hello.styles = css`
+  :host,
+  .layer {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+  }
+
+  .layer {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    font-size: 20vw;
+    font-weight: bold;
+    overflow: hidden;
+    color: white;
+    text-shadow: 0px 2vw 4vw var(--hello-shadow-1, magenta),
+      0px 2vw 1vw var(--hello-shadow-2, tomato);
+    opacity: 0.15;
+    align-items: flex-end;
+  }
+
+  .box {
+    position: relative;
+  }
+`;
 
 export const Hello = c(hello);
