32407162ae514c64e7c9bfc29ff5b6b39a078e8d
UpperCod <mtrujillo@outlook.cl>
2022-01-12 02:26:42 -0300

    check @atomico/hooks

--- src/brand/brand.stories.js
+++ src/brand/brand.stories.js
@@ -1,8 +1,7 @@
 import { Brand } from "./brand";
-// More on default export: https://storybook.js.org/docs/web-components/writing-stories/introduction#default-export
+
 export default {
   title: "components/brand",
-  // More on argTypes: https://storybook.js.org/docs/web-components/api/argtypes
   argTypes: {
     color: { control: "color" },
     width: {
@@ -13,7 +12,7 @@ export default {
 
 export const Primary = ({ color, width }) =>
   Object.assign(new Brand(), { color, width });
-// More on args: https://storybook.js.org/docs/web-components/writing-stories/args
+
 Primary.args = {
   color: "black",
   width: "280px",
@@ -21,7 +20,7 @@ Primary.args = {
 
 export const Secondary = ({ color, width }) =>
   Object.assign(new Brand(), { color, width });
-// More on args: https://storybook.js.org/docs/web-components/writing-stories/args
+
 Secondary.args = {
   color: "tomato",
   width: "280px",
