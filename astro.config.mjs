84d2b399243ad6a76b0aefe418391e1a5adc65b7
UpperCod <uppercod.git@gmail.com>
2022-09-03 09:15:57 -0400

    update vite.config.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,6 @@ import Atomico from "@atomico/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    ssr: {
-      noExternal: true,
-    },
-  },
   integrations: [Atomico({ cssLiterals: { minify: true, postcss: true } })],
 });
+.
